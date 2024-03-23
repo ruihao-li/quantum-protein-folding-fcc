03e523b80b50852afab698cd01f832e64cd3e6b9
Tomas Radivoyevitch <radivot@gmail.com>
2024-03-23 12:54:31 -0400

    Other BCC fits as MD initial conditions
    BCC9 and BCC11 failed at first step of E minimization, just like the FCC cases (maybe due to sharper than 90 deg turns). BCC8 and BCC12 gave the wrong structure, like BCC3 and BCC16.  BCC10 got it right, like BCC2.  BCC4 was close, in pocket with P18 but not P12,  RMSD of BCC10 was second highest at 1.868, so being closer may not be predictive.

--- res/tom/amber/BCCx.R
+++ res/tom/amber/BCCx.R
@@ -1,32 +1,26 @@
 system("pwd")
-# library("here")
-# setwd(here())
-bccS=c(3,4,8,9,10,11,12)    
+Sys.setenv(AMBERHOME= "/home/radivot/soft/amber22")
+system("echo $AMBERHOME")
+system("echo $PATH")
+base="/home/radivot/GH/protein-folding-qc/res/tom/amber" 
+setwd(base)
+bccS=c(3,4,8, #9,  BCC9 failed at first step of E minimization, just like the FCC cases (maybe due to sharper than 90 deg turns)
+       10,  # 11, BCC11 failed like BCC9
+       12)    
 lat="BCC"
-# i=2
-for (i in 3:length(bccS)) {
-  (ic=paste0(lat,bccS[i]))
-  base="/home/radivot/GH/protein-folding-qc/res/tom/amber" 
-  setwd(base)
-  # if (0){  # stuff to do just once, to initialize the folder
-  #   system("mkdir inputs")
-  #   system("cp DIA2/*.in inputs")  #warning, this may wipe out edits made to leap.in ... only do this block on a new folder
-  #   system("cp DIA2/*.trajin inputs") 
-  #   system("cp DIA2/*.cpptraj inputs") 
-  # }
+# k=4
+for (k in 5:length(bccS)) {
+  (ic=paste0(lat,bccS[k]))
   (baseIC=paste0(base,"/",ic))
   system(paste0("mkdir ",baseIC))
   setwd(baseIC) 
-  Sys.setenv(AMBERHOME= "/home/radivot/soft/amber22")
-  system("echo $AMBERHOME")
-  system("echo $PATH")
-  
   con=file("leap.in")
   s1="source leaprc.protein.ff19SB"
   (s2=paste0("TC5b = loadpdb ../../pulchra/pdbTC5b/",ic,".pdb"))
   s3="saveamberparm TC5b TC5b.prmtop TC5b.rst7"
   s4="quit"
   writeLines(c(s1,s2,s3,s4),con)
+  close(con)
   system("$AMBERHOME/bin/tleap -s -f leap.in > leap.out")
   system("mpirun -np 12 $AMBERHOME/bin/sander.MPI -O -i ../inputs/min1.in -o min1.out -p TC5b.prmtop -c TC5b.rst7 -r min1.ncrst")
   # Input Files: TC5b.prmtop, TC5b.rst7, min1.in        Output Files: min1.out, min1.ncrst
@@ -42,7 +36,7 @@ for (i in 3:length(bccS)) {
   setwd("analysis")
   system("../../process_mdout.perl ../heat1.out ../heat2.out ../heat3.out ../heat4.out ../heat5.out    ../heat6.out ../heat7.out ../equil1.out ../equil2.out ../equil3.out ../equil4.out ../equil5.out ../equil6.out ../equil7.out ../equil8.out ../equil9.out ../equil10.out")  
   library(readr)
-  head(d<-read_table("summary.TEMP",col_names = c("t","temp"))) 
+  # head(d<-read_table("summary.TEMP",col_names = c("t","temp"))) 
   library(ggplot2)
   library(tidyr)
   # d%>%ggplot(aes(x=t,y=temp))+geom_line()
@@ -52,12 +46,13 @@ for (i in 3:length(bccS)) {
   # d%>%ggplot(aes(x=t,y=E))+geom_line()
   library(dplyr)
   d=d%>%filter(t>50)
-  d%>%ggplot(aes(x=t,y=E))+geom_line()
+  # d%>%ggplot(aes(x=t,y=E))+geom_line()
   (tm=d%>%filter(E==min(E))) 
   system(paste0("grep ' ",tm$t[[1]],"' ../equil*.out > Emin.out"))
   
   con=file("Emin.out")
   (s=readLines(con))
+  close(con)
   (nums=as.numeric(stringr::str_extract_all(s, "\\d+")[[1]]))
   (fileNum=nums[1])
   (frameNum=nums[2]/500)
@@ -67,10 +62,10 @@ for (i in 3:length(bccS)) {
   s1=paste0("trajin equil",fileNum,".nc ",frameNum," ",frameNum)
   s2="trajout lowest_energy_struct.pdb pdb"
   writeLines(c(s1,s2),con)
-  
+  close(con)
   system("$AMBERHOME/bin/cpptraj TC5b.prmtop < extract_frame.trajin > extract_frame.out")
   
-  system("vmd lowest_energy_struct.pdb")  
+  # system("vmd lowest_energy_struct.pdb")  
   
   system("$AMBERHOME/bin/cpptraj TC5b.prmtop < ../inputs/rmsd.trajin > rmsd.out")
   
@@ -80,3 +75,10 @@ for (i in 3:length(bccS)) {
   d%>%ggplot(aes(x=t,y=rmsd))+geom_line()
   ggsave("rmsd.pdf")
 }
+
+
+# BCC9 and BCC11 failed at first step of E minimization, just like the FCC cases
+# (maybe due to sharper than 90 deg turns). BCC8 and BCC12 gave the wrong
+# structure, like BCC3 and BCC16.  BCC10 got it right, like BCC2.  BCC4 was
+# close, in pocket with P18 but not P12,  RMSD of BCC10 was second highest at
+# 1.868, so being closer may not be predictive.
