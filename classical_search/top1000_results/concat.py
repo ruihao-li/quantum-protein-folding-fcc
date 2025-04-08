59a314d227dc2a28b568680dd8bd6772eb277428
thepineapplepirate <braubeno@uno.edu>
2025-04-08 12:53:04 -0400

    chig_2mzx_vs_tomfit

--- classical_search/top1000_results/concat.py
+++ classical_search/top1000_results/concat.py
@@ -1,6 +1,7 @@
 # use this to concatenate files
 
 import os
+import pandas as pd
 
 def concatenate_files(directory, output_file):
     """
@@ -10,7 +11,7 @@ def concatenate_files(directory, output_file):
         directory (str): The path to the directory containing the files.
         output_file (str): The path to the output file where the concatenated content will be written.
     """
-    with open(output_file, 'w') as outfile:
+    with open(output_file + 'rmsd.tabular', 'w') as outfile:
         for filename in os.listdir(directory):
             file_path = os.path.join(directory, filename)
             if os.path.isfile(file_path):
@@ -18,7 +19,28 @@ def concatenate_files(directory, output_file):
                     outfile.write(infile.read())
                     outfile.write('\n')  # Add a newline between files (optional)
 
+def rmsd_stats(rmsd_df, output_file_path, protein_model):
+
+    df = pd.read_csv(rmsd_df)
+    protein_model = protein_model
+    min_rmsd = df.iloc[:,0].min().round(2)
+    mean_rmsd = df.iloc[:, 0].mean().round(2)
+    median_rmsd = df.iloc[:, 0].median().round(2)
+    std_rmsd = df.iloc[:, 0].std().round(2)
+    df_out = pd.DataFrame({'protein_model': [protein_model],
+                           'min': [min_rmsd], 
+                           'mean': [mean_rmsd],
+                           'median': [median_rmsd],
+                           'std': [std_rmsd]}
+                          )
+    df_out.to_csv(output_file_path + 'rmsd_stats.csv', index=False)
+    
+
+    # print(f"The mean of the second column is: {mean_rmsd}")
+
 # Example usage:
-directory_path = '/Users/raubenb/Desktop/gitrepos/protein-folding-qc/classical_search/top1000_results/top1000_chig_mr_1nn/RMSD_of_the_aligned_structure_collection_20062'  # Replace with the actual path to your directory
-output_file_path = '/Users/raubenb/Desktop/gitrepos/protein-folding-qc/classical_search/top1000_results/top1000_chig_mr_1nn/rmsd.tabular'  # Replace with the desired output file path
+directory_path = '/Users/raubenb/Desktop/gitrepos/protein-folding-qc/classical_search/top1000_results/top1000_2mzx_mr_1nn/TomFit/RMSD of the aligned structure collection 17103'  # Replace with the actual path to your directory
+output_file_path = '/Users/raubenb/Desktop/gitrepos/protein-folding-qc/classical_search/top1000_results/top1000_2mzx_mr_1nn/TomFit/'  # Replace with the desired output file path
+protein_model = '2mzx_MR'
 concatenate_files(directory_path, output_file_path)
+rmsd_stats(rmsd_df=output_file_path + 'rmsd.tabular', output_file_path=output_file_path, protein_model=protein_model)
