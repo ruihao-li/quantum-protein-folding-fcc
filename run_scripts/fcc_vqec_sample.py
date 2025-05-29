a01a7c3aab2d6e65bee86ec45ecc104fa7bc1828
thepineapplepirate <braubeno@uno.edu>
2025-05-29 14:47:16 -0400

    new results with vqe_c and chebyshev

--- run_scripts/fcc_vqec_sample.py
+++ run_scripts/fcc_vqec_sample.py
@@ -24,7 +24,7 @@ OPT_PARAMS_SOURCE = "klvffa_vqec_p0_5_u2/klvffa_vqec_res_12.json"
 QUBIT_NUMBER = 24
 ANSATZ_REPS = 2
 RUNNER = "hardware"  # "aer-sv", "aer-mps", "hardware"
-BACKEND_NAME = "ibm_kingston"  # change to other backends if needed, comment out if not using hardware
+BACKEND_NAME = "ibm_cleveland"  # change to other backends if needed, comment out if not using hardware
 SHOTS = 100_000
 TIMESTAMP = datetime.now(timezone.utc)
 # ============================
